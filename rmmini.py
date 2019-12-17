19ce4dc4723f91868bcdbdd6947f5516538d17a7
Eiji Tanioka <staff_ag@ping-t.com>
2019-12-18 02:01:24 +0900

    add some functions from broadlink_cli.

--- rmmini.py
+++ rmmini.py
@@ -1,3 +1,60 @@
 import broadlink
 
-class Rmmini:
+def get_device(devtype, host, mac):
+    dev = broadlink.gendevice(devtype, (host, 80), mac)
+    if dev.auth():
+        return dev
+    else
+        return False
+
+#### from broadlink_cli ####
+def __auto_int(x):
+    return int(x, 0)
+
+
+def __to_microseconds(bytes):
+    result = []
+    #  print bytes[0] # 0x26 = 38for IR
+    index = 4
+    while index < len(bytes):
+        chunk = bytes[index]
+        index += 1
+        if chunk == 0:
+            chunk = bytes[index]
+            chunk = 256 * chunk + bytes[index + 1]
+            index += 2
+        result.append(int(round(chunk * TICK)))
+        if chunk == 0x0d05:
+            break
+    return result
+
+
+def __durations_to_broadlink(durations):
+    result = bytearray()
+    result.append(IR_TOKEN)
+    result.append(0)
+    result.append(len(durations) % 256)
+    result.append(len(durations) / 256)
+    for dur in durations:
+        num = int(round(dur / TICK))
+        if num > 255:
+            result.append(0)
+            result.append(num / 256)
+        result.append(num % 256)
+    return result
+
+
+def format_durations(data):
+    result = ''
+    for i in range(0, len(data)):
+        if len(result) > 0:
+            result += ' '
+        result += ('+' if i % 2 == 0 else '-') + str(data[i])
+    return result
+
+
+def parse_durations(str):
+    result = []
+    for s in str.split():
+        result.append(abs(int(s)))
+    return result
