57070a79f053caff5f53fb5d3f7eb89d2914048a
Eiji Tanioka <tanioka404@gmail.com>
2020-03-19 23:36:25 +0900

    実際に赤外線コマンド投げられるようにした。

--- rmmini_controller.py
+++ rmmini_controller.py
@@ -23,7 +23,6 @@ async def rmmini_discovery():
     #if len(devices):
     #    for node in devices:
 
-
 @app.get("/rmmini/{rmmini_id}/command_list")
 async def rmmini_command_list(rmmini_id: int, request: Request):
     commands = db.session.query(Command).filter(Command.device_id == rmmini_id).all()
@@ -38,6 +37,16 @@ async def rmmini_learn():
     pass
 
 @app.get("/rmmini/{rmmini_id}/{func_id}/send")
-async def rmmini_send():
-    # send_data(ir_packet)
-    pass
+async def rmmini_send(request: Request, rmmini_id: int, func_id: int):
+    rmmini_info = db.session.query(Rmmini).filter(Rmmini.id == rmmini_id).first()
+    # DBのデータからホスト情報を作成
+    #mac = bytearray.fromhex(rmmini_info.mac_address)
+    device = broadlink.gendevice(rmmini_info.devtype, (rmmini_info.host, 80), bytearray.fromhex(rmmini_info.mac_address))
+    if device.auth():
+        ir_command = db.session.query(Command.ir_command).filter(
+            Command.device_id == rmmini_id,
+            Command.id == func_id).value(Command.ir_command)
+        db.session.close()
+        device.send_data(bytearray.fromhex(ir_command))
+    else:
+        return False
